b1935f3727231f8d8fa70caf035242e6261234b7
Habashy <abraha_1983@hotmail.com>
2022-11-01 11:18:13 +0200

    first commit

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const firstProject = document.getElementById('p1');
 const secondProject = document.getElementById('p2');
 const thirdProject = document.getElementById('p3');
 
-let projects = [p1, p2, p3];
-
-
 firstProject.onmouseover = () => projImage.src = './img/proj1.png';
 secondProject.onmouseover = () => projImage.src = './img/proj2.png';
 thirdProject.onmouseover = () => projImage.src = './img/proj3.png';
